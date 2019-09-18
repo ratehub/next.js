1f9e6e5467c133ddf4be109ec6079c041f74efa8
Chris Richard <chris.richard@ratehub.ca>
2019-09-17 23:08:50 -0400

    Add NewRelic instrumentation.

--- packages/next/export/worker.js
+++ packages/next/export/worker.js
@@ -6,6 +6,7 @@ import { writeFile, access } from 'fs'
 import Sema from 'async-sema'
 import AmpHtmlValidator from 'amphtml-validator'
 import { loadComponents } from 'next-server/dist/server/load-components'
+import newrelic from 'newrelic'
 
 const envConfig = require('next-server/config')
 const mkdirp = promisify(mkdirpModule)
@@ -15,6 +16,13 @@ const accessP = promisify(access)
 global.__NEXT_DATA__ = {
   nextExport: true
 }
+
+const instrument = (work, exportPathMap) => path => newrelic.startBackgroundTransaction(path, () => {
+  const { query = {} } = exportPathMap[path]
+  newrelic.addCustomAttribute('url', path)
+  newrelic.addCustomAttribute('query', query)
+  return work(path)
+})
 
 process.on(
   'message',
@@ -31,100 +39,111 @@ process.on(
     const sema = new Sema(concurrency, { capacity: exportPaths.length })
     try {
       const work = async path => {
-        await sema.acquire()
-        const ampPath = `${path === '/' ? '/index' : path}.amp`
-        const { page, query = {} } = exportPathMap[path]
-        delete query.ampOnly
-        delete query.hasAmp
-        delete query.ampPath
-        delete query.amphtml
+        try {
+          await sema.acquire()
+          const ampPath = `${path === '/' ? '/index' : path}.amp`
+          const { page, query = {} } = exportPathMap[path]
+          delete query.ampOnly
+          delete query.hasAmp
+          delete query.ampPath
+          delete query.amphtml
 
-        const req = { url: path }
-        const res = {}
-        envConfig.setConfig({
-          serverRuntimeConfig,
-          publicRuntimeConfig: renderOpts.runtimeConfig
-        })
+          const req = { url: path }
+          const res = {}
+          envConfig.setConfig({
+            serverRuntimeConfig,
+            publicRuntimeConfig: renderOpts.runtimeConfig
+          })
 
-        let htmlFilename = `${path}${sep}index.html`
-        const pageExt = extname(page)
-        const pathExt = extname(path)
-        // Make sure page isn't a folder with a dot in the name e.g. `v1.2`
-        if (pageExt !== pathExt && pathExt !== '') {
-          // If the path has an extension, use that as the filename instead
-          htmlFilename = path
-        } else if (path === '/') {
-          // If the path is the root, just use index.html
-          htmlFilename = 'index.html'
-        }
-        const baseDir = join(outDir, dirname(htmlFilename))
-        const htmlFilepath = join(outDir, htmlFilename)
+          let htmlFilename = `${path}${sep}index.html`
+          const pageExt = extname(page)
+          const pathExt = extname(path)
+          // Make sure page isn't a folder with a dot in the name e.g. `v1.2`
+          if (pageExt !== pathExt && pathExt !== '') {
+            // If the path has an extension, use that as the filename instead
+            htmlFilename = path
+          } else if (path === '/') {
+            // If the path is the root, just use index.html
+            htmlFilename = 'index.html'
+          }
+          const baseDir = join(outDir, dirname(htmlFilename))
+          const htmlFilepath = join(outDir, htmlFilename)
 
-        await mkdirp(baseDir)
-        const components = await loadComponents(distDir, buildId, page)
-        const curRenderOpts = { ...components, ...renderOpts, ampPath }
-        const html = await renderToHTML(req, res, page, query, curRenderOpts)
+          await mkdirp(baseDir)
+          const components = await loadComponents(distDir, buildId, page)
+          const curRenderOpts = { ...components, ...renderOpts, ampPath }
+          const html = await renderToHTML(req, res, page, query, curRenderOpts)
 
-        const validateAmp = async (html, page) => {
-          const validator = await AmpHtmlValidator.getInstance()
-          const result = validator.validateString(html)
-          const errors = result.errors.filter(e => e.severity === 'ERROR')
-          const warnings = result.errors.filter(e => e.severity !== 'ERROR')
+          const validateAmp = async (html, page) => {
+            const validator = await AmpHtmlValidator.getInstance()
+            const result = validator.validateString(html)
+            const errors = result.errors.filter(e => e.severity === 'ERROR')
+            const warnings = result.errors.filter(e => e.severity !== 'ERROR')
 
-          if (warnings.length || errors.length) {
-            process.send({
-              type: 'amp-validation',
-              payload: {
-                page,
-                result: {
-                  errors,
-                  warnings
+            if (warnings.length || errors.length) {
+              process.send({
+                type: 'amp-validation',
+                payload: {
+                  page,
+                  result: {
+                    errors,
+                    warnings
+                  }
                 }
-              }
-            })
+              })
+            }
           }
-        }
 
-        if (curRenderOpts.amphtml && query.amp) {
-          await validateAmp(html, path)
-        }
-        if (
-          (curRenderOpts.amphtml && !query.amp) || curRenderOpts.hasAmp
-        ) {
-          // we need to render a clean AMP version
-          const ampHtmlFilename = `${ampPath}${sep}index.html`
-          const ampBaseDir = join(outDir, dirname(ampHtmlFilename))
-          const ampHtmlFilepath = join(outDir, ampHtmlFilename)
-
-          try {
-            await accessP(ampHtmlFilepath)
-          } catch (_) {
-            // make sure it doesn't exist from manual mapping
-            const ampHtml = await renderToHTML(req, res, page, { ...query, amp: 1 }, curRenderOpts)
-
-            await validateAmp(ampHtml, page + '?amp=1')
-            await mkdirp(ampBaseDir)
-            await writeFileP(
-              ampHtmlFilepath,
-              ampHtml,
-              'utf8'
-            )
+          if (curRenderOpts.amphtml && query.amp) {
+            await validateAmp(html, path)
           }
-        }
+          if (
+            (curRenderOpts.amphtml && !query.amp) || curRenderOpts.hasAmp
+          ) {
+            // we need to render a clean AMP version
+            const ampHtmlFilename = `${ampPath}${sep}index.html`
+            const ampBaseDir = join(outDir, dirname(ampHtmlFilename))
+            const ampHtmlFilepath = join(outDir, ampHtmlFilename)
 
-        await writeFileP(
-          htmlFilepath,
-          html,
-          'utf8'
-        )
-        process.send({ type: 'progress' })
-        sema.release()
+            try {
+              await accessP(ampHtmlFilepath)
+            } catch (_) {
+              // make sure it doesn't exist from manual mapping
+              const ampHtml = await renderToHTML(req, res, page, { ...query, amp: 1 }, curRenderOpts)
+
+              await validateAmp(ampHtml, page + '?amp=1')
+              await mkdirp(ampBaseDir)
+              await writeFileP(
+                ampHtmlFilepath,
+                ampHtml,
+                'utf8'
+              )
+            }
+          }
+
+          await writeFileP(
+            htmlFilepath,
+            html,
+            'utf8'
+          )
+          process.send({ type: 'progress' })
+          sema.release()
+        } catch (err) {
+          newrelic.noticeError(err)
+          throw err
+        }
       }
-      await Promise.all(exportPaths.map(work))
+
+      const instrumentedWork = instrument(work, exportPathMap)
+
+      await Promise.all(exportPaths.map(instrumentedWork))
       process.send({ type: 'done' })
     } catch (err) {
       console.error(err)
       process.send({ type: 'error', payload: err })
+    } finally {
+      newrelic.shutdown({ collectPendingData: true })
+      console.log('Sending to NewRelic')
     }
   }
 )
