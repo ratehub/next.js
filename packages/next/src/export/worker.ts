313a3b9e514895a4a386d22525915bd3ce052f5d
Josh Lalonde <mori.ryuuji3@gmail.com>
2025-04-03 14:37:17 -0300

    feat(export): add newrelic logging to exports

--- packages/next/src/export/worker.ts
+++ packages/next/src/export/worker.ts
@@ -40,6 +40,8 @@ import {
   TurborepoAccessTraceResult,
 } from '../build/turborepo-access-trace'
 
+import newrelic from 'newrelic'
+
 const envConfig = require('../shared/lib/runtime-config.external')
 
 ;(globalThis as any).__NEXT_DATA__ = {
@@ -331,7 +333,7 @@ async function exportPageImpl(
   }
 }
 
-export default async function exportPage(
+async function exportPage(
   input: ExportPageInput
 ): Promise<ExportPageResult | undefined> {
   // Configure the http agent.
@@ -386,6 +388,28 @@ export default async function exportPage(
   }
 }
 
+const withNewRelic =
+  (work: Function) =>
+  (args: ExportPageInput): Promise<ExportPageResult | undefined> =>
+    newrelic.startBackgroundTransaction(args.pathMap.page, async () => {
+      const { path, pathMap } = args
+      const { query = {} } = pathMap
+      newrelic.addCustomAttribute('url', path)
+      newrelic.addCustomAttribute('query', serializeQuery(query))
+      const results = await work(args)
+      if (results.error) {
+        newrelic.noticeError(results.error, {
+          url: path,
+          query: serializeQuery(query),
+        })
+      }
+      return results
+    })
+
+function serializeQuery(query: any): string {
+  return query.length ? query.join(', ') : query
+}
+
 process.on('unhandledRejection', (err: unknown) => {
   // if it's a postpone error, it'll be handled later
   // when the postponed promise is actually awaited.
@@ -406,3 +430,5 @@ process.on('rejectionHandled', () => {
   // prefetching patterns to avoid waterfalls. We ignore logging these.
   // We should've already errored in anyway unhandledRejection.
 })
+
+export default withNewRelic(exportPage)
