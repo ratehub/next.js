9ad3cde4362977c12bc9c7d20f849ddb98b1d239
Josh Lalonde <mori.ryuuji3@gmail.com>
2025-06-25 15:01:03 -0300

    fix(find-page-file): only optimize in development mode

--- packages/next/src/server/lib/find-page-file.ts
+++ packages/next/src/server/lib/find-page-file.ts
@@ -131,6 +131,20 @@ export function createValidFileMatcher(
       return false
     }
 
+    // Everything below this guard is an optimization specifically for development;
+    // Omitting files during build in production mode will cause a different build hash which
+    // breaks sourcemap generation.
+    if (process.env.NODE_ENV === 'production') {
+      console.log(
+        '[find-page-file.isPagefile] Skipping page filter optimization for production mode!'
+      )
+      return true
+    } else {
+      console.log(
+        '[find-page-file.isPagefile] Using page filter optimization for development mode!'
+      )
+    }
+
     const pageFilter: string[] = process.env.EXPORT_PAGE_FILTER
       ? JSON.parse(process.env.EXPORT_PAGE_FILTER)
       : []
